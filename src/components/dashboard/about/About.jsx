fb3e604651f0b120dad940c07440275e2aac71f1
Chloe Zermatten <chloezermatten@gmail.com>
2023-11-23 20:05:28 +0000

    built dashboard about layout

--- src/components/dashboard/about/About.jsx
+++ src/components/dashboard/about/About.jsx
@@ -1,3 +1,12 @@
 export default function About() {
-  return <section id="display-about-me"></section>;
+  return (
+    <>
+      <section id="display-about-me">
+          <div className="round-container about" id="about-dance">15 year dancing</div>
+          <div className="round-container about" id="about-teach">4 years teaching</div>
+          <div className="round-container about" id="about-code">3 months coding</div>
+          <div className="round-container about" id="about-study">5 years studying</div>
+      </section>
+    </>
+  );
 }
