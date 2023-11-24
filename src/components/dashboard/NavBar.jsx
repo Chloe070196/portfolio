9698ea27bfe7f9987c6f8c64e32aa81d47f9ff77
Chloe Zermatten <chloezermatten@gmail.com>
2023-11-24 06:02:05 +0000

    make h2 responsive

--- src/components/dashboard/NavBar.jsx
+++ src/components/dashboard/NavBar.jsx
@@ -5,14 +5,14 @@ export default function NavBar({ setDisplay }) {
     <>
       <nav className="section-container">
         <div className="rectangular-container tech-stack">
-        <h2>tech stack : HTML, CSS, JS, React + Vite</h2>
+        <h2>tech stack : HTML, CSS, JS, React + Vite, GitHub/  Git, VSCode</h2>
         </div>
         <button
           className="rectangular-container nav"
           value="display-projects"
           onClick={(e) => toggleDisplay(e, setDisplay)}
         >
-          <h2>things I've done so far</h2>
+          <h2>things I've made so far</h2>
         </button>
         <button
           className="rectangular-container nav"
